5487b4fcf26f5cd5f507a96b26f800b1c8d6c2e3
Ajij_Khaan <ajijkhaan28@gmail.com>
2021-09-02 17:18:52 +0600

    done

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 
 // Fetching Data
 const fetchData = () => {
+    //Load CUrrese Search Term
     const searchText = document.getElementById('search-input');
-    // console.log(searchText);
-
     const url = `https://openlibrary.org/search.json?q=${searchText.value}`;
+
+    //Clear Previous Search Term
     searchText.value = '';
+
     fetch(url)
         .then(res => res.json())
         .then(data => loadData(data))
@@ -13,37 +15,35 @@ const fetchData = () => {
 
 //Loading Data
 const loadData = (data) => {
-    //Total Result Found Function
-    totalResult(data)
-    // console.log(data.docs);
+    //Total Result Found 
+    showingResult(data)
 
+
+    // console.log(data.docs);
+    //Clear the previous books display
     const bookContainer = document.getElementById('show-books-container');
     bookContainer.innerHTML = '';
 
-    //Book Loder Funtion
+    //Display New books
     data.docs.forEach(element => {
-        showData(element);
+        displayBook(element);
     });
 }
 
-//Showing Data
-const showData = (data) => {
+//Disply books
+const displayBook = (data) => {
 
-
-    // console.log(urlCover);
     const urlCover = `https://covers.openlibrary.org/b/id/${data.cover_i}-M.jpg`;
     const bookTitle = data.title;
     const author = data.author_name;
     const firstPublished = data.first_publish_year;
 
-    // console.log(bookTitle, firstPublished);
-
     const bookContainer = document.getElementById('show-books-container');
-
     const div = document.createElement('div');
+
     div.innerHTML = `
             <div class="col mt-3">
-            <div class="card h-100">
+            <div class="card h-100 border border-1 border-primary">
             
             <img src="${urlCover}"  class="card-img-top img-size" alt="...">
         
@@ -51,7 +51,7 @@ const showData = (data) => {
                     <small class="text-dark fw-bold">${bookTitle}</small>
                 </div>
                 <div class="card-footer">
-                    <small class="text-muted">By ${author}</small>
+                    <small class="text-muted">Author: ${author}</small>
                 </div>
                 <div class="card-footer">
                     <small class="text-muted">First published in ${firstPublished}</small>
@@ -61,25 +61,27 @@ const showData = (data) => {
         `;
 
     bookContainer.appendChild(div);
-
 }
 
-const totalResult = (data) => {
+//Checking how much resutl showing
+const showingResult = (data) => {
     const totalSearchResult = document.getElementById('total-search-result');
     const noBookFound = document.getElementById('no-book-found');
 
     if (data.numFound === 0) {
         // console.log('NO Book Found. Please Search Again.')
+        totalSearchResult.classList.add('d-none');
         noBookFound.classList.remove('d-none');
     }
 
     else {
         totalSearchResult.classList.remove('d-none');
-        // console.log(data.numFound);
+
+        //counting how much books is showing
         let count = 0;
-        data.docs.forEach(element => {
-            count++;
-        });
+        data.docs.forEach(element => count++);
+
         totalSearchResult.innerText = `Showing ${count} of ${parseInt(data.numFound)} books`;
+        noBookFound.classList.add('d-none');
     }
 }
